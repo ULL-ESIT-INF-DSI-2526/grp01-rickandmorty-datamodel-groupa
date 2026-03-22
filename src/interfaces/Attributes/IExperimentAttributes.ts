59de87f73df08c5b8157da44b27ddc8cb3de153c
Karen Santos <alu0101677354@ull.edu.es>
2026-03-22 13:08:38 +0000

    Corrección interfaz experiment

--- src/interfaces/Attributes/IExperimentAttributes.ts
+++ src/interfaces/Attributes/IExperimentAttributes.ts
@@ -1,13 +1,23 @@
 import { Character } from "../../classes/Character.js"
 import { Dimension } from "../../classes/Dimension.js";
 import { ExperimentState, ExperimentType} from "../../types/ExperimentState.js";
+import { GeneralAttributes } from "./IGeneralAttributes.js";
 
-export interface ExperimentAttributes {
-    id: string;
-    name: string;
+/**
+ * Representa los atributos que definen un Experimento.
+ * 
+ * Esta interfaz describe la estructura de los datos necesarios
+ * para crear o representar un experimento del multiverso.
+ */
+export interface ExperimentAttributes extends GeneralAttributes {
+    /** Descripción detallada del experimento */
     description: string;
+    /** Personaje que creó el experimento */
     creator: Character;
+    /** Estado actual del experimento */
     state: ExperimentState;
+    /** Dimensión en la que se llevó a cabo el experimento. */
     originDimension: Dimension;
+    /** Tipo de experimento si crea una dimensión o si la destruye */
     type: ExperimentType;
 }
